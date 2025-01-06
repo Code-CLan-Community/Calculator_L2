8f419daa1e8e4eed46be701bbdc051e8726d1809
Code-Clan-Community <veergupta0801@gmail.com>
2025-01-06 21:09:16 +0530

    Adding file Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+public class Calculator
+{
+
+	public static void main(String[] args) 
+	{
+     	System.out.println("Welcome To Calculator!");
+
+
+	}
+}
