b4c8128b454924a074ca28209c8f3fa652d31dbe
Code-Clan-Community <veergupta0801@gmail.com>
2025-01-07 10:53:08 +0530

    #2 adding Sub

--- Calculator.java
+++ Calculator.java
@@ -28,6 +28,9 @@ public class Calculator
         case '+':
             o = num1 + num2;
             break;
+	case '-':
+            o = num1 - num2;
+            break;
 	 default:
             System.out.println("You enter wrong input");
         }
